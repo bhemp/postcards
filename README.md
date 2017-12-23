505564829aa3832a1a0363c75b12f4185a5bcf88
Billy Hempstead <william.hempstead@outlook.com>
2017-12-23 17:08:58 -0500

    First Major Update

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
-#postcards
+# Vintage Postcard Generator
 
-Vintage Postcard Generator
+The Vintage Postcard Generator allows a user to quickly generate a retro-styled postcard using their own photos.
 
 
+I originally envisioned this as a native mobile app, but it currently only uses HTML, CSS, and Javascript/JQuery. I've started this as a final project for a Intro to Computer Science class. As my skills increase, I can see this being greatly improved by implementing a more advanced JavaScript or mobile framework.
+
+## Getting Started
+
+Head to the project link:
+
+https://bhemp.github.io/postcards/
+
+### Prerequisites
+
+N/A
+
+### Installing
+
+N/A
+
+## Testing
+
+N/A
+
+## TODOs
+
+* Add options for changing fonts
+* Add options to change secondary font color
+* Finish implementation of border selection
+* Improve function of image export - currently printing to PDF to preserve formatting - tried printing canvas with mixed success
+* Improve overall UX/UI
+
+## Built With
+
+* HTML5
+* CSS3
+* JavaScript
+* Bootstrap
+* JQuery
+* Googling
+
+## Authors
+
+* **Billy Hempstead** - *Initial work* - https://bhemp.github.io/
--- js/jscript.js
+++ js/jscript.js
@@ -1,82 +1,171 @@
 $(document).ready(function () {
 
-    $('#generate').click(function () {
-        var locationValue = $('#location').val();
-        var introValue = $('#intro').val();
+    var mainImgRadioToggle = 'image';
+    var mainTextRadioToggle = 'image';
 
-        $('#locationText').html(locationValue);
-        $('#introText').html(introValue);
+    //    BACKGROUND
+    //    ---------------------
+
+    //    MAIN BACKGROUND COLOR
+    // Get main background color value from color picker
+
+    var mainBGColor = '#FFFFFF';
+    $('#mainImgColor').change(function () {
+        mainBGColor = '#' + this.value;
+        if (mainImgRadioToggle == 'color') {
+            $('#mainColor').css('background-color', mainBGColor);
+        }
+    });
+
+    //    CHECK RADIO BUTTON STATUS
+    //    Controls the radio button choice between a background image and solid color
+
+    $('input[type=radio][name=mainImgRadio]').change(function () {
+        if (this.value == 'image') {
+            mainImgRadioToggle = 'image';
+
+        } else if (this.value == 'color') {
+            mainImgRadioToggle = 'color';
+        }
+    });
+
+    //    Change solid color opacity based on radio click
+    $('#RadioMC').click(function () {
+        $('#mainColor').css('background-color', mainBGColor);
+        $('#mainColor').css('opacity', '1');
+    });
+
+    $('#RadioMI').click(function () {
+        $('#mainColor').css('opacity', '0');
+
+    });
+
+    //    Changes the main background image after choosing a local file
+    $('#mainImgFilename').change(function () {
+        if (mainImgRadioToggle == 'image') {
+            if (this.files && this.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#mainImg').attr('src', e.target.result);
+                    $('#workspace.color').css('opacity', '1');
+
+                }
+                reader.readAsDataURL(this.files[0]);
+            }
+        }
+    });
+
+    //  TEXT APPEARANCE
+    //  ----------------------
+
+    var mainTextColor = '#FFFFFF';
+    $('#mainTextColor').change(function () {
+        mainTextColor = '#' + this.value;
+        if (mainTextRadioToggle == 'color') {
+            $('#mainText').css('-webkit-text-fill-color', mainTextColor);
+        }
+    });
+
+    var outlineColor = '#FFFFFF';
+    $('#outlineColor').change(function () {
+        outlineColor = '#' + this.value;
+        $('#mainText').css('-webkit-text-stroke-color', outlineColor);
     });
 
 
-});
+    //    CHECK RADIO BUTTON STATUS
+    //    Controls the radio button choice between a background image and solid color
 
-function updateLHPS(val) {
-    document.getElementById('locHPText').innerHTML = val;
-    document.getElementById('locationText').style.left = val + '%';
+    $('input[type=radio][name=mainTextRadio]').change(function () {
+        if (this.value == 'image') {
+            mainTextRadioToggle = 'image';
+
+        } else if (this.value == 'color') {
+            mainTextRadioToggle = 'color';
+        }
+    });
+
+
+    //    Change solid color opacity based on radio click
+    $('#RadioTC').click(function () {
+        $('#mainText').css('-webkit-text-fill-color', mainTextColor);
+
+    });
+
+    $('#RadioTI').click(function () {
+        $('#mainText').css('-webkit-text-fill-color', 'transparent');
+    });
+
+
+    //    Changes the main text background image after choosing a local file
+    $('#mainTextFilename').change(function () {
+        if (mainTextRadioToggle == 'image') {
+            var file = document.getElementById('mainTextFilename').files[0];
+            var reader = new FileReader();
+            mainTextImgPath = "url(" + reader.result + ")";
+            reader.onloadend = function () {
+                document.getElementById('mainText').style.backgroundImage = "url(" + reader.result + ")";
+            }
+            if (file) {
+                reader.readAsDataURL(file);
+            } else {}
+        }
+    });
+
+    //  TEXT VALUES
+    //  ------------------------
+    //    Updates the image text when the "Update" button is clicked
+    $('#update').click(function () {
+        var mainTextValue = $('#mainTextInput').val();
+        var secTextValue = $('#secTextInput').val();
+
+        $('#mainText').html(mainTextValue);
+        $('#secText').html(secTextValue);
+    });
+
+    //  IMAGE EXPORT
+    //  -----------------------------
+    //    It would appear that there isn't a simple JS way to export the rendered canvas to an image file while preserving all of the image features (e.g. text clipping). For this implementation, printing to PDF seems to be the best way to preserve the rendered image.
+
+    $('#export').click(function () {
+        var mode = 'iframe'; //popup
+        var close = mode == "popup";
+        var options = {
+            mode: mode,
+            popClose: close
+        };
+        $("div.printArea").printArea(options);
+    });
+
+}); // end of document.ready
+
+
+function updateMHPS(val) {
+    document.getElementById('MHPSlider').innerHTML = val;
+    document.getElementById('mainText').style.left = val + '%';
 }
 
-function updateLVPS(val) {
-    document.getElementById('locVPText').innerHTML = val;
-    document.getElementById('locationText').style.top = val + '%';
+function updateMVPS(val) {
+    document.getElementById('MVPSlider').innerHTML = val;
+    document.getElementById('mainText').style.top = val + '%';
 }
 
-function updateLFSS(val) {
-    document.getElementById('locFSText').innerHTML = val;
-    document.getElementById('locationText').style.fontSize = val + 'px';
+function updateMFSS(val) {
+    document.getElementById('MFSSlider').innerHTML = val;
+    document.getElementById('mainText').style.fontSize = val + 'px';
 }
 
-function previewFile() {
-    // Where you will display your image
-    var preview = document.querySelector('bgImg');
-    // The button where the user chooses the local image to display
-    var file = document.querySelector('input[type=file]').files[0];
-    // FileReader instance
-    var reader = new FileReader();
-
-    // When the image is loaded we will set it as source of
-    // our img tag
-    reader.onloadend = function () {
-        preview.src = reader.result;
-    }
-
-
-    if (file) {
-        // Load image as a base64 encoded URI
-        reader.readAsDataURL(file);
-    } else {
-        preview.src = "";
-    }
+function updateSHPS(val) {
+    document.getElementById('SHPSlider').innerHTML = val;
+    document.getElementById('secText').style.left = val + '%';
 }
 
+function updateSVPS(val) {
+    document.getElementById('SVPSlider').innerHTML = val;
+    document.getElementById('secText').style.top = val + '%';
+}
 
-//window.onload = addListeners;
-//
-//function addListeners() {
-//    document.getElementById('locationText').addEventListener('mousedown', mouseDown, false);
-//    window.addEventListener('mouseup', mouseUp, false);
-//
-//}
-//
-//function mouseUp() {
-//    window.removeEventListener('mousemove', divMove, true);
-//}
-//
-//function mouseDown(e) {
-//    window.addEventListener('mousemove', divMove, true);
-//}
-//
-//function divMove(e) {
-//    var div = document.getElementById('locationText');
-//    div.style.top = e.clientY + 'px';
-//    div.style.left = e.clientX + 'px';
-//}
-
-//
-//$('#locVPSlider').onchange(function () {
-//    var locVPValue = $('#locVPSlider').val();
-//    $('#locVPText').html(locHPValue);
-//});
-
-
-//<!--https://stackoverflow.com/questions/11855781/jquery-getting-data-from-form-->
+function updateSFSS(val) {
+    document.getElementById('SFSSlider').innerHTML = val;
+    document.getElementById('secText').style.fontSize = val + 'px';
+}
